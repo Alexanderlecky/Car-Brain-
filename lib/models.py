6d610c45f11e49db0fbfd9eee88cbb65d7399b8f
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-09-19 14:24:55 +0300

    :memo:

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, Date
+# models.py
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,46 +7,46 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
-
-    vehicles = relationship('Vehicle', back_populates='owner')
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    vehicles = relationship('Vehicle', back_populates='user')
 
 class Vehicle(Base):
     __tablename__ = 'vehicles'
-    vehicle_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'))
-    make = Column(String)
-    model = Column(String)
-    year = Column(Integer)
-    vin = Column(String)
-    current_mileage = Column(Integer)
-    purchase_date = Column(Date)
-
-    owner = relationship('User', back_populates='vehicles')
+    
+    id = Column(Integer, primary_key=True)
+    make = Column(String, nullable=False)
+    model = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    vin = Column(String, unique=True, nullable=False)
+    current_mileage = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    
+    user = relationship('User', back_populates='vehicles')
     maintenance_logs = relationship('MaintenanceLog', back_populates='vehicle')
     expenses = relationship('Expense', back_populates='vehicle')
 
 class MaintenanceLog(Base):
-    __tablename__ = 'maintenance_log'
-    maintenance_id = Column(Integer, primary_key=True)
-    vehicle_id = Column(Integer, ForeignKey('vehicles.vehicle_id'))
-    task = Column(String)
-    date = Column(Date)
-    mileage = Column(Integer)
+    __tablename__ = 'maintenance_logs'
+    
+    id = Column(Integer, primary_key=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    task = Column(String, nullable=False)
+    date = Column(Date, nullable=False)
+    mileage = Column(Integer, nullable=False)
     description = Column(String)
-
+    
     vehicle = relationship('Vehicle', back_populates='maintenance_logs')
 
 class Expense(Base):
     __tablename__ = 'expenses'
-    expense_id = Column(Integer, primary_key=True)
-    vehicle_id = Column(Integer, ForeignKey('vehicles.vehicle_id'))
-    expense_type = Column(String)
-    amount = Column(Float)
-    date = Column(Date)
-    description = Column(String)
-
+    
+    id = Column(Integer, primary_key=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    amount = Column(Float, nullable=False)
+    description = Column(String, nullable=False)
+    date = Column(Date, nullable=False)
+    
     vehicle = relationship('Vehicle', back_populates='expenses')
